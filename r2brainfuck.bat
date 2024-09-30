4711d3f3a2eabc465f2ab910fe2a0cd60dc12162
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-09-30 20:08:43 +0300

    Add files via upload

---
+++ r2brainfuck.bat
@@ -0,0 +1,7 @@
+SET REFAL=F:\Programming\refal2
+
+%REFAL%\bin\refal2 r2brainfuck > r2brainfuck.log
+as -o r2brainfuck.o r2brainfuck.asm >> r2brainfuck.log
+gcc -o r2brainfuck.exe %REFAL%\lib\r2objects\mainrf.o %REFAL%\lib\r2libext\r2libext.o r2brainfuck.o -Wl,-L%REFAL%\lib -Wl,-lrefal2,-lr2user >> r2brainfuck.log
+r2brainfuck
+
