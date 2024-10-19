ed108691c1df7e53ad47a06bfb048fe216df1634
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-19 22:21:24 +0300

    Update RefalABbrainfuck.bat

--- RefalABbrainfuck.bat
+++ RefalABbrainfuck.bat
@@ -2,7 +2,7 @@ SET REFAL=F:\Programming\Projects\RefalAB
 
 %REFAL%\bin\RefalAB RefalABBrainfuck > RefalABBrainfuck.log
 as -o RefalABBrainfuck.o RefalABBrainfuck.asm >> RefalABBrainfuck.log
-gcc -pipe -Wall -O2 -I%REFAL%\include -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
-gcc -o RefalABBrainfuck.exe %REFAL%\lib\mainrf.o RefalABBrainfuck.o RefalABBrainfuckc.o -Wl,-L%REFAL%\lib -Wl,-lRefalAB >> RefalABBrainfuck.log
+clang -pipe -Wall -O2 -I%REFAL%\include -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
+clang -o RefalABBrainfuck.exe %REFAL%\lib\mainrf.o RefalABBrainfuck.o RefalABBrainfuckc.o -Wl,-L%REFAL%\lib -Wl,-lRefalAB >> RefalABBrainfuck.log
 RefalABBrainfuck
 
