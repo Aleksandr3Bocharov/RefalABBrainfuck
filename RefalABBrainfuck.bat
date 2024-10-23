507ce3e83426b9ec3aee3c23ead6be2dbf3ef44d
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-23 22:27:26 +0300

    Add files via upload

--- RefalABBrainfuck.bat
+++ RefalABBrainfuck.bat
@@ -3,11 +3,11 @@
 rem Copyright 2024 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-10-22
+rem 2024-10-23
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
 %REFALABBIN%\RefalAB RefalABBrainfuck > RefalABBrainfuck.log
 as -o RefalABBrainfuck.o RefalABBrainfuck.asm >> RefalABBrainfuck.log
 clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
-clang -o RefalABBrainfuck.exe %REFALABLIB%\mainrf.o RefalABBrainfuck.o RefalABBrainfuckc.o -L%REFALABLIB% -lRefalAB >> RefalABBrainfuck.log
+clang -o RefalABBrainfuck.exe RefalABBrainfuck.o RefalABBrainfuckc.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB >> RefalABBrainfuck.log
 RefalABBrainfuck
--- RefalABBrainfuck.sh
+++ RefalABBrainfuck.sh
@@ -3,13 +3,11 @@
 # Copyright 2024 Aleksandr Bocharov
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-# 2024-10-22
+# 2024-10-23
 # https://github.com/Aleksandr3Bocharov/RefalAB
 
-REFAL=/media/alexb/F/Programming/Projects/RefalAB
-
-$REFAL/bin/RefalAB RefalABBrainfuck > RefalABBrainfuck.log
+$REFALABBIN/RefalAB RefalABBrainfuck > RefalABBrainfuck.log
 as -o RefalABBrainfuck.o RefalABBrainfuck.asm >> RefalABBrainfuck.log
-clang -pipe -Wall -O2 -I$REFAL/include -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
-clang -o RefalABBrainfuck $REFAL/lib/mainrf.o RefalABBrainfuck.o RefalABBrainfuckc.o -L$REFAL/lib -lRefalAB >> RefalABBrainfuck.log
+clang $REFALABCFLAGS -I$REFALABINCLUDE -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
+clang -o RefalABBrainfuck RefalABBrainfuck.o RefalABBrainfuckc.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB >> RefalABBrainfuck.log
 ./RefalABBrainfuck
