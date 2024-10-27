71861d2e682b708753a0604514502d65d3a5b74d
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2024-10-27 17:36:16 +0200

    Add files via upload

--- RefalABBrainfuckc.c
+++ RefalABBrainfuckc.c
@@ -1,7 +1,7 @@
 // Copyright 2024 Aleksandr Bocharov
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-// 2024-10-20
+// 2024-10-27
 // https://github.com/Aleksandr3Bocharov/RefalABBrainfuck
 
 //====================================================================
@@ -15,13 +15,14 @@
 static void putch_(void)
 {
     const T_LINKCB *p = refal.preva->next;
-    if (p->tag == TAGN && p->next == refal.nexta)
-    {
-        putchar((int)gcoden(p));
-        fflush(stdout);
-    }
-    else
-        refal.upshot = 2;
+    if (p != refal.nexta)
+        if (p->tag == TAGN && p->next == refal.nexta)
+        {
+            putchar((int)gcoden(p));
+            fflush(stdout);
+            return;
+        }
+    refal.upshot = 2;
     return;
 }
 char putch_0[] = {Z5 'P', 'U', 'T', 'C', 'H', '\005'};
@@ -32,13 +33,17 @@ void (*putch_1)(void) = putch_;
 static void getch_(void)
 {
     if (refal.preva->next != refal.nexta)
+    {
         refal.upshot = 2;
+        return;
+    }
     T_LINKCB *p = refal.prevr;
     const int c = getchar();
     if (!slins(p, 1))
         return;
     p = p->next;
     p->tag = TAGN;
+    p->info.codep = NULL;
     if (c != EOF)
         pcoden(p, (uint8_t)c);
     return;
