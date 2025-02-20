1025e636363e34a9db4a89d47804049afc82ddab
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-02-20 10:34:43 +0200

    Update RefalABBrainfuck_gui.bat

--- RefalABBrainfuck_gui.bat
+++ RefalABBrainfuck_gui.bat
@@ -1,13 +1,13 @@
 @echo off
 
-rem Copyright 2024 Aleksandr Bocharov
+rem Copyright 2025 Aleksandr Bocharov
 rem Distributed under the Boost Software License, Version 1.0.
 rem See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-rem 2024-10-23
+rem 2025-02-20
 rem https://github.com/Aleksandr3Bocharov/RefalAB
 
-%REFALABBIN%\RefalAB RefalABBrainfuck > RefalABBrainfuck.log
-as -o RefalABBrainfuck.o RefalABBrainfuck.asm >> RefalABBrainfuck.log
-clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
-clang -o RefalABBrainfuck.exe RefalABBrainfuck.o RefalABBrainfuckc.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB >> RefalABBrainfuck.log
-RefalABBrainfuck
+%REFALABBIN%\RefalAB RefalABBrainfuck_gui > RefalABBrainfuck_gui.log
+as -o RefalABBrainfuck_gui.o RefalABBrainfuck_gui.asm >> RefalABBrainfuck_gui.log
+clang %REFALABCFLAGS% -I%REFALABINCLUDE% -c RefalABBrainfuckc_gui.c -o RefalABBrainfuckc_gui.o >> RefalABBrainfuck_gui.log
+clang -o RefalABBrainfuck_gui.exe RefalABBrainfuck_gui.o RefalABBrainfuckc_gui.o %REFALABLIB%\mainrf.o -L%REFALABLIB% -lRefalAB >> RefalABBrainfuck_gui.log
+RefalABBrainfuck_gui
