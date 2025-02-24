dc542640e6b127b4a8aa4aaf856898e2336ce445
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-02-24 11:59:52 +0200

    Add files via upload

--- gui.c
+++ gui.c
@@ -12,7 +12,7 @@
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
-#undef RAYGUI_IMPLEMENTATION 
+#undef RAYGUI_IMPLEMENTATION
 
 #define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
 #include "gui_window_file_dialog.h"
@@ -21,24 +21,62 @@
 
 void guiwindow(void)
 {
-    InitWindow(400, 200, "raygui - controls test suite");
+    // Initialization
+    //---------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 560;
+    InitWindow(screenWidth, screenHeight, "raygui - custom modal dialog");
+    SetExitKey(0);
+    // Custom file dialog
+    GuiWindowFileDialogState fileDialogState = InitGuiWindowFileDialog(GetWorkingDirectory());
+    bool exitWindow = false;
+    char fileNameToLoad[512] = {0};
+    Texture texture = {0};
     SetTargetFPS(60);
-    bool showMessageBox = false;
-    while (!WindowShouldClose())
+    //--------------------------------------------------------------------------------------
+    // Main game loop
+    while (!exitWindow) // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+        exitWindow = WindowShouldClose();
+        if (fileDialogState.SelectFilePressed)
+        {
+            // Load image file (if supported extension)
+            if (IsFileExtension(fileDialogState.fileNameText, ".png"))
+            {
+                strcpy(fileNameToLoad, TextFormat("%s" PATH_SEPERATOR "%s", fileDialogState.dirPathText, fileDialogState.fileNameText));
+                UnloadTexture(texture);
+                texture = LoadTexture(fileNameToLoad);
+            }
+            fileDialogState.SelectFilePressed = false;
+        }
+        //----------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
         ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-        if (GuiButton((Rectangle){24, 24, 120, 30}, "#191#Show Message"))
-            showMessageBox = true;
-        if (showMessageBox)
-        {
-            int result = GuiMessageBox((Rectangle){85, 70, 250, 100},
-                                       "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
-
-            if (result >= 0)
-                showMessageBox = false;
-        }
+        DrawTexture(texture, GetScreenWidth() / 2 - texture.width / 2, GetScreenHeight() / 2 - texture.height / 2 - 5, WHITE);
+        DrawRectangleLines(GetScreenWidth() / 2 - texture.width / 2, GetScreenHeight() / 2 - texture.height / 2 - 5, texture.width, texture.height, BLACK);
+        DrawText(fileNameToLoad, 208, GetScreenHeight() - 20, 10, GRAY);
+        // raygui: controls drawing
+        //----------------------------------------------------------------------------------
+        if (fileDialogState.windowActive)
+            GuiLock();
+        if (GuiButton((Rectangle){20, 20, 140, 30}, GuiIconText(ICON_FILE_OPEN, "Open Image")))
+            fileDialogState.windowActive = true;
+        GuiUnlock();
+        // GUI: Dialog Window
+        //--------------------------------------------------------------------------------
+        GuiWindowFileDialog(&fileDialogState);
+        //--------------------------------------------------------------------------------
+        //----------------------------------------------------------------------------------
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadTexture(texture); // Unload texture
+    CloseWindow();          // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 }
