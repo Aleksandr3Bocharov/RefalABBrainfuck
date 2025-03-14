ca1cd6216ba7f394d84517df61cbc56873c75edc
Aleksandr Bocharov <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-03-14 10:49:38 +0200

    Update RefalABBrainfuck_gui.sh

--- RefalABBrainfuck_gui.sh
+++ RefalABBrainfuck_gui.sh
@@ -10,7 +10,7 @@ RAYLIBCFLAGS="-Wall -O2"
 RAYLIBLFLAGS="-lraylib -lm"
 
 $REFALABBIN/RefalAB RefalABBrainfuck_gui > RefalABBrainfuck_gui.log
-as -o RefalABBrainfuck_gui.o RefalABBrainfuck_gui.asm >> RefalABBrainfuck_gui.log
+as -o RefalABBrainfuck_gui.o RefalABBrainfuck_gui.s >> RefalABBrainfuck_gui.log
 clang $REFALABCFLAGS -I$REFALABINCLUDE -c RefalABBrainfuckc_gui.c -o RefalABBrainfuckc_gui.o >> RefalABBrainfuck_gui.log
 clang $RAYLIBCFLAGS -c gui.c -o gui.o >> RefalABBrainfuck_gui.log
 clang -o RefalABBrainfuck_gui RefalABBrainfuck_gui.o RefalABBrainfuckc_gui.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB gui.o $RAYLIBLFLAGS >> RefalABBrainfuck_gui.log
