d69ade4ada7470e735d606568d2562b81e6fa327
Александр Бочаров <134220754+Aleksandr3Bocharov@users.noreply.github.com>
2025-04-10 20:02:12 +0300

    Add files via upload

--- RefalABBrainfuck.sh
+++ RefalABBrainfuck.sh
@@ -3,11 +3,11 @@
 # Copyright 2025 Aleksandr Bocharov
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-# 2025-03-14
+# 2025-04-10
 # https://github.com/Aleksandr3Bocharov/RefalAB
 
 $REFALABBIN/RefalAB RefalABBrainfuck > RefalABBrainfuck.log
 as -o RefalABBrainfuck.o RefalABBrainfuck.s >> RefalABBrainfuck.log
 clang $REFALABCFLAGS -I$REFALABINCLUDE -c RefalABBrainfuckc.c -o RefalABBrainfuckc.o >> RefalABBrainfuck.log
 clang -o RefalABBrainfuck RefalABBrainfuck.o RefalABBrainfuckc.o $REFALABLIB/mainrf.o -L$REFALABLIB -lRefalAB >> RefalABBrainfuck.log
-./RefalABBrainfuck
+./RefalABBrainfuck $1
